edd07d6446e927c3980dc50bd3104e9a487b2447
Kathleen Edie <kathleen.edie@arnoldclark.com>
2023-11-03 16:49:23 +0000

    Setting up page layout

---
+++ src/app/blog/[id]/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogLayout = ({ children }) => {
+  return (
+    <div>
+        <h1>My mini blog layout</h1>
+        { children }
+    </div>
+  )
+}
+
+export default BlogLayout
